3593ebe1860f1b7e5eedc5ea522ab3964e1734b4
Mahir <98490297+Mahir-Mamaliyev@users.noreply.github.com>
2022-08-11 00:56:59 +0400

    experience

---
+++ src/components/Experience.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className="experience">
+        <div className="d-flex justify-content-center my-5">
+            <h1>Experience</h1>
+        </div>
+        <div className="container experience-wrapper">
+            
+            <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2012-2013</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur corporis ab vitae nobis quidem reprehenderit saepe exercitationem pariatur aliquid culpa.</p>
+                </div>
+            </div>
+
+            <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2013-2014</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa atque dicta tempora maiores aut perspiciatis non ducimus suscipit vel! Numquam voluptates praesentium fugit. Ratione, provident!</p>
+                </div>
+            </div>
+
+            <div className="timeline-block timeline-block-right">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2015-2017</h3>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro error, excepturi consequuntur repellat atque distinctio exercitationem ipsam sed voluptates odit odio illo cum ea mollitia officia!</p>
+                </div>
+            </div>
+
+            <div className="timeline-block timeline-block-left">
+                <div className="marker"></div>
+                <div className="timeline-content">
+                    <h3>2017-2021</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis modi, earum fugit eaque ad beatae iste ratione sint. Sequi ab totam libero officiis temporibus?</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Experience
